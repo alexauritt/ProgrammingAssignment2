c784caf4db9145d5a594059f3c6f7011ef8227fb
Alex Auritt <alexauritt@yahoo.com>
2014-11-18 23:35:56 -0500

    better commenting

--- cachematrix.R
+++ cachematrix.R
@@ -1,28 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## calculates a special 'matrix', represented by a list containing
+## the following four functions to:
+##   1) set the value of the matrix
+##   2) get the value of the matrix
+##   3) set the inverse of the matrix (i.e. cache the inverse)
+##   4) get the (cached) inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
+	
 	# private storage of inverse, initially set to NULL
 	# ... as inverse has not yet been calculcated
 	inverse <- NULL
 	
 	# function allowing value of matrix to be reassigned.
 	# we reset inverse to NULL as inverse will need to be
-	# recalculated for new matrix value
+	# recalculated for new matrix
 	set <- function(y) {
 		x <<- y
 		inverse <<- NULL
 	}
 
-	# getter method - retrieve current matrix value
+	# getter method - retrieve current matrix
 	get <- function() x
 
-	# we'll use this to cache inverse after calculation
+	# setter to cache inverse after calculation
 	setinverse <- function(inv) inverse <<- inv
 
-	# getter for caculated inverse
+	# getter for pre-caculated inverse (may be null)
 	getinverse <- function() inverse
 	
 	# return list of 4 functions defined above
@@ -32,17 +34,22 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+	# retrieve cached inverse of x (may be null)  
 	m <- x$getinverse()
+
+	# if cached value is not null, use it, and print msg
 	if(!is.null(m)) {
 		message("getting cached data")
 		return(m)
 	}
+
+	# otherwise,retrieve matrix value, compute and store inverse
 	data <- x$get()
 	m <- solve(data, ...)
 	x$setinverse(m)
+
+	# return newly computed inverse
 	m
 }
