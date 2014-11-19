0b31e7ffde305c656e46b0d555ae2be2ec345281
Alex Auritt <alexauritt@yahoo.com>
2014-11-18 22:19:48 -0500

    better commenting

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,28 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	# private storage of inverse, initially set to NULL
+	# ... as inverse has not yet been calculcated
 	inverse <- NULL
+	
+	# function allowing value of matrix to be reassigned.
+	# we reset inverse to NULL as inverse will need to be
+	# recalculated for new matrix value
 	set <- function(y) {
 		x <<- y
 		inverse <<- NULL
 	}
+
+	# getter method - retrieve current matrix value
 	get <- function() x
+
+	# we'll use this to cache inverse after calculation
 	setinverse <- function(inv) inverse <<- inv
+
+	# getter for caculated inverse
 	getinverse <- function() inverse
+	
+	# return list of 4 functions defined above
 	list(set = set, get = get, 
 		setinverse = setinverse, 
 		getinverse = getinverse)
@@ -26,8 +40,6 @@ cacheSolve <- function(x, ...) {
 	if(!is.null(m)) {
 		message("getting cached data")
 		return(m)
-	} else {
-		message("no cached data")
 	}
 	data <- x$get()
 	m <- solve(data, ...)
